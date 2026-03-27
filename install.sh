cf1955e8a4335b0245a0fbcf299aaa73d8cbbd3f
BISHT.cx <bisht-ji@outlook.com>
2026-03-27 09:13:18 +0530

    Revert "Add install.sh"
    This reverts commit 9c18e3860755dc0ad08d672cba58dbc9e158b3fc.

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing Focus-Symphony..."
-go build -o focus-symphony main.go
-mkdir -p ~/.local/bin
-mv focus-symphony ~/.local/bin/
-echo "Done! Run 'focus-symphony' from anywhere."
